da722bfe9e93d0386d0ad0369600aa57e1627857
kalkidanh <kalkidanh89@gmail.com>
2022-07-02 08:18:07 -0700

    no q or e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
