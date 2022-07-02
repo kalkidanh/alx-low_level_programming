776b6d9027846c4fef252f7ee63c4438db51d019
kalkidanh <kalkidanh89@gmail.com>
2022-07-02 09:08:29 -0700

    single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number ='0'; number <= '9'; number++)
+	{
+		putchar(number);
+		if (number == '9')
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
