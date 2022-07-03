099bcd3dcfa6e2645f3ac6564753da9914347f71
kalkidanh <kalkidanh89@gmail.com>
2022-07-03 07:16:43 -0700

    3 numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number1, number2, number3;
+
+	for (number1 = 0; number1 < 9; number1++)
+	{
+		for (number2 = number1 + 1; number2 < 9; number2++)
+		{
+			for (number3 = number2 + 1; number3 < 10; number3++)
+			{
+				putchar((number1 % 10) + '0');
+				putchar((number2 % 10) + '0');
+				putchar((number3 % 10) + '0');
+				if (number1 == 7 && number2 == 8 && number3 == 9)
+					continue;
+				putchar(',');
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.s
@@ -0,0 +1,135 @@
+	.file	"101-print_comb4.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$0, -12(%rbp)
+	jmp	.L2
+.L9:
+	movl	-12(%rbp), %eax
+	addl	$1, %eax
+	movl	%eax, -8(%rbp)
+	jmp	.L3
+.L8:
+	movl	-8(%rbp), %eax
+	addl	$1, %eax
+	movl	%eax, -4(%rbp)
+	jmp	.L4
+.L7:
+	movl	-12(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	$2, %edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	leal	48(%rdx), %eax
+	movl	%eax, %edi
+	call	putchar@PLT
+	movl	-8(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	$2, %edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	leal	48(%rdx), %eax
+	movl	%eax, %edi
+	call	putchar@PLT
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	$2, %edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	leal	48(%rdx), %eax
+	movl	%eax, %edi
+	call	putchar@PLT
+	cmpl	$7, -12(%rbp)
+	jne	.L5
+	cmpl	$8, -8(%rbp)
+	jne	.L5
+	cmpl	$9, -4(%rbp)
+	je	.L11
+.L5:
+	movl	$44, %edi
+	call	putchar@PLT
+	movl	$44, %edi
+	call	putchar@PLT
+	jmp	.L6
+.L11:
+	nop
+.L6:
+	addl	$1, -4(%rbp)
+.L4:
+	cmpl	$9, -4(%rbp)
+	jle	.L7
+	addl	$1, -8(%rbp)
+.L3:
+	cmpl	$8, -8(%rbp)
+	jle	.L8
+	addl	$1, -12(%rbp)
+.L2:
+	cmpl	$8, -12(%rbp)
+	jle	.L9
+	movl	$10, %edi
+	call	putchar@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
