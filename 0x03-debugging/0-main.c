f24426c48803f077fa84c1a6fd5f727b2028eebf
kalkidanh <kalkidanh89@gmail.com>
2022-07-07 07:58:29 -0700

    pos or neg or 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -print if the number is positive or negative
+ * or zero
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
