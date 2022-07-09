71c43ea7c6b85eb14772493f2b5085992c6e6432
kalkidanh <kalkidanh89@gmail.com>
2022-07-09 04:51:31 -0700

    zero times fourteen

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.s
@@ -0,0 +1,91 @@
+	.file	"5-more_numbers.c"
+	.text
+	.globl	more_numbers
+	.type	more_numbers, @function
+more_numbers:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$0, -8(%rbp)
+	jmp	.L2
+.L6:
+	movl	$0, -4(%rbp)
+	jmp	.L3
+.L5:
+	cmpl	$9, -4(%rbp)
+	jle	.L4
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	imulq	$1717986919, %rdx, %rdx
+	shrq	$32, %rdx
+	sarl	$2, %edx
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	addl	$48, %eax
+	movsbl	%al, %eax
+	movl	%eax, %edi
+	call	_putchar@PLT
+.L4:
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	$2, %edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	sall	$2, %eax
+	addl	%edx, %eax
+	addl	%eax, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %edx
+	movl	%edx, %eax
+	addl	$48, %eax
+	movsbl	%al, %eax
+	movl	%eax, %edi
+	call	_putchar@PLT
+	addl	$1, -4(%rbp)
+.L3:
+	cmpl	$14, -4(%rbp)
+	jle	.L5
+	movl	$10, %edi
+	call	_putchar@PLT
+	addl	$1, -8(%rbp)
+.L2:
+	cmpl	$9, -8(%rbp)
+	jle	.L6
+	nop
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	more_numbers, .-more_numbers
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
