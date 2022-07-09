949d4d25b9ee7e3fb361231356c3a4fcd87946c1
kalkidanh <kalkidanh89@gmail.com>
2022-07-09 09:33:55 -0700

    fizz and buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - for multiples of 3 and 5 print 1 to 100
+ * wil print 'fizz buzz'
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.s
@@ -0,0 +1,152 @@
+	.file	"9-fizz_buzz.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"FizzBuzz"
+.LC1:
+	.string	"Fizz"
+.LC2:
+	.string	"Buzz"
+.LC3:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$1, -4(%rbp)
+	jmp	.L2
+.L8:
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1431655766, %rax, %rax
+	shrq	$32, %rax
+	movq	%rax, %rdx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	movl	%edx, %esi
+	subl	%eax, %esi
+	movl	%esi, %eax
+	movl	%eax, %edx
+	addl	%edx, %edx
+	addl	%eax, %edx
+	movl	%ecx, %eax
+	subl	%edx, %eax
+	testl	%eax, %eax
+	jne	.L3
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	%edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movl	%eax, %edx
+	sall	$2, %edx
+	addl	%eax, %edx
+	movl	%ecx, %eax
+	subl	%edx, %eax
+	testl	%eax, %eax
+	jne	.L3
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L4
+.L3:
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1431655766, %rax, %rax
+	shrq	$32, %rax
+	movq	%rax, %rdx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	movl	%edx, %esi
+	subl	%eax, %esi
+	movl	%esi, %eax
+	movl	%eax, %edx
+	addl	%edx, %edx
+	addl	%eax, %edx
+	movl	%ecx, %eax
+	subl	%edx, %eax
+	testl	%eax, %eax
+	jne	.L5
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L4
+.L5:
+	movl	-4(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	%edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movl	%eax, %edx
+	sall	$2, %edx
+	addl	%eax, %edx
+	movl	%ecx, %eax
+	subl	%edx, %eax
+	testl	%eax, %eax
+	jne	.L6
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L4
+.L6:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L4:
+	cmpl	$100, -4(%rbp)
+	je	.L7
+	movl	$32, %edi
+	call	putchar@PLT
+.L7:
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$100, -4(%rbp)
+	jle	.L8
+	movl	$10, %edi
+	call	putchar@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
