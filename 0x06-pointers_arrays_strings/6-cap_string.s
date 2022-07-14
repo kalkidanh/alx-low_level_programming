8f8fc21af1f5266fbfb206ad922a5b932b62da24
kalkidanh <kalkidanh89@gmail.com>
2022-07-14 09:23:55 -0700

    capitalism

--- 0x06-pointers_arrays_strings/6-cap_string.s
+++ 0x06-pointers_arrays_strings/6-cap_string.s
@@ -1,1 +1,184 @@
 	.file	"6-cap_string.c"
+	.text
+	.globl	cap_string
+	.type	cap_string, @function
+cap_string:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L4:
+	addl	$1, -4(%rbp)
+.L3:
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$96, %al
+	jle	.L4
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$122, %al
+	jg	.L4
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$32, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$9, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$10, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$44, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$59, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$46, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$33, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$63, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$34, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$40, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$41, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$123, %al
+	je	.L5
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	-1(%rax), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	cmpb	$125, %al
+	jne	.L2
+.L5:
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	leal	-32(%rax), %ecx
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	%ecx, %edx
+	movb	%dl, (%rax)
+.L2:
+	addl	$1, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movslq	%eax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	testb	%al, %al
+	jne	.L3
+	movq	-24(%rbp), %rax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	cap_string, .-cap_string
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
