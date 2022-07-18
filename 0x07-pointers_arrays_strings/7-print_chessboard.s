0e974ea6015ced2aa377a85ec2344572b928d5bb
kalkidanh <kalkidanh89@gmail.com>
2022-07-18 14:54:08 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.s
@@ -0,0 +1,71 @@
+	.file	"7-print_chessboard.c"
+	.text
+	.globl	print_chessboard
+	.type	print_chessboard, @function
+print_chessboard:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)
+	movl	$0, -8(%rbp)
+	jmp	.L2
+.L5:
+	movl	$0, -4(%rbp)
+	jmp	.L3
+.L4:
+	movl	-8(%rbp), %eax
+	cltq
+	leaq	0(,%rax,8), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-4(%rbp), %eax
+	cltq
+	addq	%rdx, %rax
+	movzbl	(%rax), %eax
+	movsbl	%al, %eax
+	movl	%eax, %edi
+	movl	$0, %eax
+	call	_putchar@PLT
+	addl	$1, -4(%rbp)
+.L3:
+	cmpl	$7, -4(%rbp)
+	jle	.L4
+	movl	$10, %edi
+	movl	$0, %eax
+	call	_putchar@PLT
+	addl	$1, -8(%rbp)
+.L2:
+	cmpl	$7, -8(%rbp)
+	jle	.L5
+	nop
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	print_chessboard, .-print_chessboard
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
