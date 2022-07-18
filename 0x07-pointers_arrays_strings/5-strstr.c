ab745d6a436a3f8f93570dbbff4aae2360a08851
kalkidanh <kalkidanh89@gmail.com>
2022-07-18 14:36:12 -0700

    needle in a heystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - find first sub string
+ * @haystack: string to search
+ * @needle: sub string
+ *
+ * Return: a pointer or 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
