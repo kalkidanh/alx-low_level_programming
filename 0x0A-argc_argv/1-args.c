fc3e383857b746c478c40e3c3be05639b25b9527
kalkidanh <kalkidanh89@gmail.com>
2022-07-22 08:06:48 -0700

    arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments
+ * @argc: number of arguments
+ * @argv: name of array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.s
@@ -0,0 +1,72 @@
+	.file	"1-args.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	cmpl	$1, -20(%rbp)
+	jne	.L2
+	movl	-20(%rbp), %eax
+	subl	$1, %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L3
+.L2:
+	movl	$0, -4(%rbp)
+	jmp	.L4
+.L5:
+	addl	$1, -4(%rbp)
+	addq	$8, -32(%rbp)
+.L4:
+	movq	-32(%rbp), %rax
+	movq	(%rax), %rax
+	testq	%rax, %rax
+	jne	.L5
+	movl	-4(%rbp), %eax
+	subl	$1, %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L3:
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
