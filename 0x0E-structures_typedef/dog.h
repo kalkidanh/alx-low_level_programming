0b48219b4424e8cceabaa1675976e070b6774ec8
kalkidanh <kalkidanh89@gmail.com>
2022-08-01 16:20:36 -0700

    dog printed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,13 +15,13 @@ struct dog
 };
 
 /**
- * dog_ti - Typedef for dog
+ * dog_t - Typedef for dog
  */
-typedef struct dog dog_ti;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_ti *new_dog(char *name, float age, char *owner);
-void free_dog(dog_ti *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
