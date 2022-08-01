28d5db92cbda0ae2f5f711a4de5943a73f5f0dd0
kalkidanh <kalkidanh89@gmail.com>
2022-08-01 11:12:22 -0700

    absolute values

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-
-/**
- * File: 3-function_like_macro.h
- * Desc: define ABS(x) to find the value of x.
- *
- */
-
 #define ABS(x) ((x) < 0 ? (-x) : (x))
-
 #endif
