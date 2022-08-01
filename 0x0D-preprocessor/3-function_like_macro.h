ff040abf86016f63c05eefbc273f90831f227470
kalkidanh <kalkidanh89@gmail.com>
2022-08-01 10:54:52 -0700

    absolute valued

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro.h
+ * Desc: define ABS(x)
+ *
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
