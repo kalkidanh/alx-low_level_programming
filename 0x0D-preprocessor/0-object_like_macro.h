baf1fcba54600ed8e626a7ae0129b98c43f2143f
kalkidanh <kalkidanh89@gmail.com>
2022-08-01 10:32:27 -0700

    macro defined

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-objec_like_macro.h
+ * Desc: define a macro named SIZE as abbr for the token 1024
+ *
+ */
+
+#define SIZE 1024
+
+#endif
