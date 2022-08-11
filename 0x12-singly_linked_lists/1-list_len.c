f797d9707355038eb14bf461c39e85a60f02cc3b
kalkidanh <kalkidanh89@gmail.com>
2022-08-11 09:24:03 -0700

    elements counted

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - number of elements in linked list
+ * @h: pointer to the list
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+
+	return (elements);
+}
