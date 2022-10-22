31f3b5688b101ce3274fb26274e3ff3b21fdd757
kalkidanh <kalkidanh89@gmail.com>
2022-10-22 08:39:20 -0700

    djb2 2

---
+++ 0x1A-hash_tables/2-key_index.s
@@ -0,0 +1,46 @@
+	.file	"2-key_index.c"
+	.text
+	.globl	key_index
+	.type	key_index, @function
+key_index:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	hash_djb2@PLT
+	movl	$0, %edx
+	divq	-16(%rbp)
+	movq	%rdx, %rax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	key_index, .-key_index
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
