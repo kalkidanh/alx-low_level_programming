7d0092cdab9c4661db349050d559a1cca57a691c
kalkidanh <kalkidanh89@gmail.com>
2022-12-18 10:12:35 -0800

    prints everything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,109 @@
+#include "variadic_functions.h"
+
+/**
+ * print_char - print a character
+ * @arg: list of arguments pointing to the string
+ */
+void print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+
+	printf("%c", letter);
+}
+
+/**
+ * print_int - print the integer
+ * @arg: list of arguments pointing to the string
+ */
+
+void print_int(va_list arg)
+{
+	int num;
+
+	num = va_arg(arg, int);
+
+	printf("%d", num);
+}
+
+
+/**
+ * print_float - print the floating point number
+ * @arg: list of arguments pointing to the string
+ */
+void print_float(va_list arg)
+{
+	float num;
+
+	num = va_arg(arg, double);
+
+	printf("%f", num);
+}
+
+
+
+/**
+ * print_string - print the string
+ * @arg: list of arguments pointing to the string
+ */
+void print_string(va_list arg)
+{
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", str);
+}
+
+/**
+ * print_all - print all
+ * @format: input format
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+
+	int i = 0, j = 0;
+
+	char *separator = "";
+
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(args, format);
+
+	while (format && (*(format + i)))
+	{
+		j = 0;
+
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
+		}
+
+		i++;
+
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
