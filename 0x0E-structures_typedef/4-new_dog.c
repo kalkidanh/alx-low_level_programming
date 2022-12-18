2505ee758661961857907245ff0c089f98246ddc
OZYMAYNDIAS <kalkidanh89@gmail.com>
2022-12-18 17:21:37 +0300

    dog created and freed

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,77 @@
+#include "dog.h"
+
+
+/**
+*_strlen - return the length of string
+*@str: string to count
+*Return: length of string
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (str)
+len++;
+
+return (len);
+}
+
+
+/**
+*_strcopy - copy string pointed by src into dest
+*@dest:string copy buffer
+*@src: string to copy buffer
+*Return:return string copy
+*/
+char *_strcopy(char *dest, char *src)
+{
+int index = 0;
+
+for (; src[index] ; index++)
+dest[index] = src[index];
+
+dest[index] = '\0';
+return (dest);
+}
+
+
+
+
+/**
+*new_dog - create a new dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*Return: return new dog or NULL
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *doggo;
+
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+
+doggo = malloc(sizeof(dog_t));
+if (doggo == NULL)
+return (NULL);
+
+doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (doggo->name == NULL)
+{
+free(doggo);
+return (NULL);
+}
+
+doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (doggo->owner == NULL)
+{
+free(doggo->name);
+free(doggo);
+return (NULL);
+}
+
+doggo->name = _strcopy(doggo->name, name);
+doggo->age = age;
+doggo->owner = _strcopy(doggo->owner, owner);
+
+return (doggo);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,34 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - type to describe a dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * dog_t - Typedef for dog
- */
+*struct dog - data struct for dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+/**
+*dog_t - Typedef for struct dog
+*/
 typedef struct dog dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 
 #endif
