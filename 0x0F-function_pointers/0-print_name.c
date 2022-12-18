8d1bd7bc98ced05cf58818f0eebb789d8f376711
kalkidanh <kalkidanh89@gmail.com>
2022-12-18 08:13:47 -0800

    name printed

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name
+ * @name: name to print
+ * @f: function to print name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
