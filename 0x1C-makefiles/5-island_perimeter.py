e1eab3e7fd1b305fdbdb3347a5abd4507c0b013f
kalkidanh <kalkidanh89@gmail.com>
2023-03-22 21:38:28 +0300

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains the function island_perimeter
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island
+
+    Args:
+    grid (list): list of list of integers
+
+    Returns:
+    int: perimeter of the island
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
